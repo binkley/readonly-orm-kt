9d6870cc46c24f80c4c77eabfecb965809e02acb
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-07-15 16:36:02 -0500

    Create from SQL results

--- src/test/kotlin/hm/binkley/labs/a/AInputRecord.kt
+++ src/test/kotlin/hm/binkley/labs/a/AInputRecord.kt
@@ -3,7 +3,11 @@ package hm.binkley.labs.a
 import hm.binkley.labs.input.HasBazCount
 import hm.binkley.labs.input.HasFooId
 import hm.binkley.labs.input.InputRecord
+import java.sql.ResultSet
 
 data class AInputRecord(override val fooId: String,
                         override val bazCount: Int)
-    : InputRecord, HasFooId, HasBazCount
+    : InputRecord, HasFooId, HasBazCount {
+    constructor(results: ResultSet)
+            : this(results.getString("fooId"), results.getInt("bazCount"))
+}
