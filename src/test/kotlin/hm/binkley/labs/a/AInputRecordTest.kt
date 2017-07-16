fcf13ff058a1e1a07e872c6449b3777a49b4e65a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-07-15 19:58:29 -0500

    Test for construct from SQL

---
+++ src/test/kotlin/hm/binkley/labs/a/AInputRecordTest.kt
@@ -0,0 +1,23 @@
+package hm.binkley.labs.a
+
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.junit.MatcherAssert.assertThat
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.eq
+import org.mockito.Mockito.mock
+import java.sql.ResultSet
+
+class AInputRecordTest {
+    @Test
+    fun shouldConstruct() {
+        val results = mock(ResultSet::class.java)
+        `when`(results.getString(eq("fooId"))).
+                thenReturn("FUBAR lives!")
+        `when`(results.getInt(eq("bazCount"))).
+                thenReturn(3)
+
+        assertThat(AInputRecord(results),
+                `is`(AInputRecord("FUBAR lives!", 3)))
+    }
+}
