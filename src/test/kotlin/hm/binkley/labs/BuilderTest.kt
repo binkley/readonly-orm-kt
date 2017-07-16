3350540f12824786d0b2488d8b7676fcde168ea2
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-07-16 08:22:50 -0500

    Real builder test
    Next decide how to pull out the building code to a class or function.

--- src/test/kotlin/hm/binkley/labs/BuilderTest.kt
+++ src/test/kotlin/hm/binkley/labs/BuilderTest.kt
@@ -4,15 +4,23 @@ import hm.binkley.labs.a.AInputRecord
 import hm.binkley.labs.b.BOutputRecord.Companion
 import hm.binkley.labs.input.InputRecord
 import hm.binkley.labs.output.OutputRecord
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.junit.MatcherAssert.assertThat
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.eq
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
+import java.io.File
 import java.sql.ResultSet
 
 class BuilderTest {
+    @Rule @JvmField
+    val tmpdir = TemporaryFolder()
+
     @Test
     fun shouldBuild() {
         val results = mock(ResultSet::class.java)
@@ -23,24 +31,31 @@ class BuilderTest {
         `when`(results.getInt(eq("bazCount"))).
                 thenReturn(3, 4)
 
-        build(results, ::AInputRecord, Companion::asBOutputRecord) {
-            println(it.joinToString("|"))
-        }
+        val file = tmpdir.newFile()
+
+        build(results, ::AInputRecord, Companion::asBOutputRecord, file)
 
         verify(results, times(1)).close()
+
+        assertThat(file.readLines(), `is`(listOf(
+                "A|Bar marker?|3|3 × A",
+                "B|Bar marker?|4|4 × B")))
     }
 
     private fun <I : InputRecord, O : OutputRecord> build(
             results: ResultSet,
             toInputRecord: (ResultSet) -> I,
             toOutputRecord: (I) -> O,
-            action: (List<*>) -> Unit) {
-        results.use {
-            ResultSetIterator(results).asSequence().
-                    map(toInputRecord).
-                    map(toOutputRecord).
-                    map { it.fields() }.
-                    forEach(action)
+            file: File) {
+        results.use { input ->
+            file.printWriter().use { output ->
+                ResultSetIterator(input).asSequence().
+                        map(toInputRecord).
+                        map(toOutputRecord).
+                        map { it.fields() }.
+                        map { it.joinToString("|") }.
+                        forEach { output.println(it) }
+            }
         }
     }
 
