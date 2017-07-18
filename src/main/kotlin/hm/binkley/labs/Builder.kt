873dc49d36b4d52d5014669da3bb689ae7460177
Brian Oxley <boxley@thoughtworks.com>
2017-07-18 19:50:13 -0400

    Begin detangling file

--- src/main/kotlin/hm/binkley/labs/Builder.kt
+++ src/main/kotlin/hm/binkley/labs/Builder.kt
@@ -3,21 +3,33 @@ package hm.binkley.labs
 import hm.binkley.labs.input.InputRecord
 import hm.binkley.labs.output.OutputRecord
 import java.io.File
+import java.io.Writer
 import java.sql.ResultSet
+
+fun <I : InputRecord, O : OutputRecord> build(
+        results: ResultSet,
+        toInputRecord: (ResultSet) -> I,
+        toOutputRecord: (I) -> O,
+        write: (O) -> Unit) = results.use { input ->
+    ResultSetIterator(input).asSequence().
+            map(toInputRecord).
+            map(toOutputRecord).
+            forEach(write)
+}
 
 fun <I : InputRecord, O : OutputRecord> build(
         results: ResultSet,
         file: File,
         toInputRecord: (ResultSet) -> I,
         toOutputRecord: (I) -> O,
-        toLine: (List<*>) -> String) = results.use { input ->
-    file.bufferedWriter().use { output ->
-        ResultSetIterator(input).asSequence().
-                map(toInputRecord).
-                map(toOutputRecord).
-                map { it.fields() }.
-                map(toLine).
-                forEach { output.append(it); output.append('\n') }
+        toLine: (List<*>) -> String) = file.bufferedWriter().use { output ->
+    build(results, toInputRecord, toOutputRecord, writeTo(output, toLine))
+}
+
+private fun <O : OutputRecord> writeTo(output: Writer, toLine: (List<*>) -> String): (O) -> Unit = {
+    output.run {
+        append(toLine(it.fields()))
+        append('\n')
     }
 }
 
